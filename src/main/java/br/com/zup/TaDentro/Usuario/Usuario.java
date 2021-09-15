c8a3279faa30a4f38ab29d0a6cf33c2e67b8a0df
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-15 16:32:48 -0300

    Classe usuario com seus atributos

--- src/main/java/br/com/zup/TaDentro/Usuario/Usuario.java
+++ src/main/java/br/com/zup/TaDentro/Usuario/Usuario.java
@@ -1,4 +1,54 @@
 package br.com.zup.TaDentro.Usuario;
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String email;
+    private String Cargo;
+
+    public Usuario() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCargo() {
+        return Cargo;
+    }
+
+    public void setCargo(String cargo) {
+        Cargo = cargo;
+    }
 }
